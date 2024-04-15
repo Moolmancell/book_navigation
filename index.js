0e38162d5c32baad03e641f3b97046e8c4b279aa
Moolmancell <richmondclaudebodot@gmail.com>
2024-04-16 01:26:13 +0800

    Add functionality in forum controls

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ const book_author =  document.getElementById("book_author");
 const book_pages =  document.getElementById("book_pages");
 const book_status = document.getElementById("read_status");
 const submit_button = document.getElementById("submitButton");
+const cancel_button = document.getElementById("cancelButton");
 
 addBook.addEventListener("click", (e) => {
     newBookForm.classList.toggle("hidden");
-
 })
 
-submit_button.addEventListener("click", (e) => {
-    e.preventDefault();
-    
-})
+submit_button.addEventListener("click", addBookToLibrary)
+
+cancel_button.onclick = function() {
+    newBookForm.classList.toggle("hidden");
+    emptyField();
+}
 
 const myLibrary = [];
 
@@ -28,6 +30,18 @@ function Book(title, author, pages, status) {
   this.status = status;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(e) {
   // do stuff here
+  const book = new Book(book_title.value, book_author.value, book_pages.value, book_status.checked);
+    e.preventDefault();
+    newBookForm.classList.toggle("hidden");
+    emptyField();
+    myLibrary.push(book);
+}
+
+function emptyField() {
+    book_title.value = "";
+    book_author.value = "";
+    book_pages.value = "";
+    book_status.checked = false;
 }
